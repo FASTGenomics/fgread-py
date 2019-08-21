96a19160420d535029684802eaabc257d5289bd9
Paweł Biernat <pawel.biernat@comma-soft.de>
2019-08-21 10:30:41 +0200

    fixed a test

--- tests/test_list.py
+++ tests/test_list.py
@@ -4,4 +4,4 @@ import fgread
 def test_list(data_dir):
     dsets_list = fgread.list_datasets(data_dir)
     assert len(dsets_list) == 5
-    assert list(dsets_list.keys()) == [1, 2, 3, 4, 5]
+    assert set(dsets_list.keys()) == set([1, 2, 3, 4, 5])
