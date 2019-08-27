2ca2a339f52842da5456c633f81f153b1822d046
Benedikt Reiz <LustigePerson@gmx.de>
2019-08-27 14:16:06 +0200

    Fix test again

--- tests/test_anndata.py
+++ tests/test_anndata.py
@@ -8,8 +8,8 @@ def test_read_single_dataset(dset, list_datasets):
     assert adata.shape == dset["dim"]
     assert adata.uns["metadata"]["title"] == dset["title"]
     assert adata.uns["metadata"]["format"] == dset["format"]
-    assert (adata.obs["id"] == dset["id"]).all()
-    assert (adata.obs["title"] == dset["title"]).all() 
+    assert (adata.obs["fg_id"] == dset["id"]).all()
+    assert (adata.obs["fg_title"] == dset["title"]).all() 
 
 def test_seurat_raises(dset_seurat, list_datasets):
     with pytest.raises(NotImplementedError):
