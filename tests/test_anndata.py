7e90d2ead27848fb35c9f8fb66bd9222f0280270
PapenMi <Michael.von.Papen@comma-soft.com>
2019-09-26 15:44:06 +0200

    added mtx, read from scanpy, PEP8

--- tests/test_anndata.py
+++ tests/test_anndata.py
@@ -9,7 +9,8 @@ def test_read_single_dataset(dset, list_datasets):
     assert adata.uns["metadata"]["title"] == dset["title"]
     assert adata.uns["metadata"]["format"] == dset["format"]
     assert (adata.obs["fg_id"] == dset["id"]).all()
-    assert (adata.obs["fg_title"] == dset["title"]).all() 
+    assert (adata.obs["fg_title"] == dset["title"]).all()
+
 
 def test_seurat_raises(dset_seurat, list_datasets):
     with pytest.raises(NotImplementedError):
