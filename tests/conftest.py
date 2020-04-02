09a9a8c07be978dd1396a27265350766b1f2f99c
Benedikt <benedikt.reiz@comma-soft.com>
2020-04-02 14:29:17 +0200

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,16 @@
-import pytest
-import pandas as pd
-from pathlib import Path
 import os
+from pathlib import Path
 
 import fgread
+import pandas as pd
+import pytest
 
 HERE = Path(__file__).parent
 DATA_DIR = HERE / "data" / "all_datasets"
 DSETS = os.listdir(DATA_DIR)
 
-DATASETS = [
-    dict(id=1, dim=(298, 16892), title="Loom dataset", format="Loom"),
-    dict(id=3, dim=(10, 20), title="AnnData dataset", format="AnnData"),
-    dict(id=4, dim=(1222, 33538), title="10x (hdf5) dataset", format="10x (hdf5)"),
-    dict(id=5, dim=(20000, 99), title="tab-separated text dataset", format="tab-separated text"),
-    dict(id=8, dim=(30, 1000), title="mtx legacy dataset", format="10x (mtx)"),
-    dict(id=9, dim=(30, 1000), title="mtx v3 dataset", format="10x (mtx)"),
-    dict(id=10, dim=(499, 99), title="comma-separated text dataset", format="comma-separated text"),
-    dict(id=11, dim=(499, 99), title="tab-separated text variant dataset", format="tab-separated text"),
-]
-
-# special treatment for the unsupported seurat object
-DATASET_SEURAT = dict(
-    id=2, dim=(1222, 33538), title="Seurat Object dataset", format="Seurat Object"
-)
-
-# special treatment for  "other dataset"
-DATASET_OTHER = dict(id=6, title="Other dataset", format="Other")
-
-# special treatment for "not set dataset"
-DATASET_NOTSET = dict(id=7, title="Not set dataset", format="Not set")
+DATASETS = fgread.ds_info(data_dir=DATA_DIR, output=True, pretty=False)
+N_DATASETS = DATASETS.shape[0]
 
 
 @pytest.fixture
@@ -37,34 +18,21 @@ def data_dir():
     return DATA_DIR
 
 
-# a list of all datasets
-@pytest.fixture()
-def list_datasets(data_dir):
-    return fgread.ds_info(data_dir=data_dir)
-
-
-# supported datasets
-@pytest.fixture(params=DATASETS)
+@pytest.fixture(params=list(range(N_DATASETS)))
 def dset(request):
-    return request.param
-
-
-# The seurat dataset
-@pytest.fixture
-def dset_seurat():
-    return DATASET_SEURAT
-
-
-# The notset dataset
-@pytest.fixture
-def dset_notset():
-    return DATASET_NOTSET
+    return DATASETS.loc[request.param]
 
 
 # read json from all datasets
 @pytest.fixture(params=DSETS)
 def json_dset(request):
-    series = pd.read_json(str(DATA_DIR) + "/" + request.param + "/dataset_info.json",
-                          typ="series")
+    series = pd.read_json(
+        str(DATA_DIR) + "/" + request.param + "/dataset_info.json", typ="series"
+    )
     df = series.to_frame().transpose()
     return df
+
+
+@pytest.fixture
+def list_datasets():
+    return fgread.ds_info(data_dir=DATA_DIR, output=True, pretty=False)
--- tests/test_anndata.py
+++ tests/test_anndata.py
@@ -1,18 +1,20 @@
 import pytest
 import fgread
 
-
-def test_read_anndata(data_dir, list_datasets):
-    adata = fgread.load_data("AnnData dataset", data_dir=data_dir)
-    list_data = list_datasets[list_datasets["title"] == "AnnData dataset"]
-    n_cells, n_genes = adata.X.shape
-    print(adata.uns["ds_metadata"])
-    assert n_genes == list_data["numberOfGenes"].values
-    assert n_cells == list_data["numberOfCells"].values
-    assert list(adata.uns["ds_metadata"]) == list_data["id"].values
-    assert adata.obs["fg_id"][0] == list_data["id"].values
+load_fail = {
+    "Seurat Object dataset": {"type": NotImplementedError},
+    "No expression": {"type": TypeError},
+    "Other dataset": {"type": KeyError},
+    "mtx legacy dataset": {"type": KeyError},
+    "mtx v3 dataset": {"type": KeyError},
+    "Multifile dataset meta": {"type": TypeError},
+}
 
 
-def test_seurat_raises(data_dir, list_datasets):
-    with pytest.raises(NotImplementedError):
-        fgread.load_data("Seurat Object dataset", data_dir=data_dir)
+def test_read_anndata(data_dir, dset):
+    title = dset["title"]
+    if title in load_fail:
+        with pytest.raises(load_fail[title]["type"]):
+            fgread.load_data(title, data_dir=data_dir)
+    else:
+        pass
