ef8ea6f1692593ac7e1dda9a65565cafca044d3b
Benedikt Reiz <41583627+LustigePerson@users.noreply.github.com>
2020-04-03 13:35:09 +0200

    Multifile support
    * ds_info check
    
    * ds_info(ds) check
    
    * nicer ds-detail view
    
    * 1st draft load_data()
    
    * update test data
    
    * update to new testdata
    
    * update testdata
    
    * update reference data
    
    * check json schmea to handle old reading functions
    
    * update loading for multifile
    
    * update tests
    
    * add h5 mapping to 10x hdf5
    
    Co-authored-by: mvonpapen <26275671+mvonpapen@users.noreply.github.com>

--- tests/test_list.py
+++ tests/test_list.py
@@ -11,6 +11,6 @@ def test_list(data_dir, list_datasets):
 def test_representation(json_dset, list_datasets):
     json_data = json_dset.drop("schemaVersion", axis=1)
     title = json_data["title"].values[0]
-    list_data = list_datasets[list_datasets["title"] == title]
+    list_data = list_datasets.loc[list_datasets["title"] == title]
     for col in json_data.columns:
         assert list_data[col].values == json_data[col].values
